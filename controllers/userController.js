6ef7680189eef7b79743d6b45fdf982e3652f5e8
Mountian Driver <lsfreigenberg@outlook.com>
2022-10-14 10:41:27 -0600

    Create Pesudo code for controllers folder

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { Thought, Reaction, User } = require('../models/');
+
+
+module.exports = {
+// /api/users 
+    
+    // GET all users
+
+    // GET a single user by its _id and populated thought and friend data
+
+    // POST a new user
+
+    // PUT to update a user by its _id
+
+    // DELETE to reomve user by its _id
+
+    // BONUS remove user's associated thoughts when deleted
+
+
+// /api/users/:userId/friends/:friendId
+
+    // POST to add new friend to a user's friend list
+
+    // DELETE to remove a friend from a user's friend list
+}
